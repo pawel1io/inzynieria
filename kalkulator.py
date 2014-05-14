34d5726d32803e208d9b67e8d7bd1e5a67e800ba
Pawel <a@wp.pl>
2014-05-14 15:46:18 +0200

    dodalem funkcje get_info

--- kalkulator.py
+++ kalkulator.py
@@ -1,5 +1,9 @@
+def get_info():
+    print('To jest prosty kalkulator')
+
 def dodawanie():
     a=int(input('Wprowadz pierwsza liczbe '))
     b=int(input('Wprowadz druga liczbe '))
     print(a+b)
+get_info()
 dodawanie()
